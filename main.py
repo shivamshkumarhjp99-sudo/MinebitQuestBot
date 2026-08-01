7bd76411f4d82ae4e4d837c1b4c94b283cc3f5b5
Shivam <YOUR_GITHUB_EMAIL>
2026-08-01 06:39:48 +0530

    Fix GitHub Actions

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-import time
 import traceback
 
 from checker import get_quests
@@ -7,54 +6,53 @@ from telegram_bot import send_message
 
 print("🚀 Minebit Quest Bot Started...")
 
-while True:
+def main():
+    current = get_quests()
 
     try:
-        current = get_quests()
+        with open("storage.json", "r", encoding="utf-8") as f:
+            old = json.load(f)
+    except:
+        old = []
 
-        try:
-            with open("storage.json", "r", encoding="utf-8") as f:
-                old = json.load(f)
-        except (FileNotFoundError, json.JSONDecodeError):
-            old = []
+    old_names = set()
 
-        old_names = set()
+    for item in old:
+        if isinstance(item, dict):
+            old_names.add(item["name"])
+        elif isinstance(item, str):
+            old_names.add(item)
 
-        for item in old:
-            if isinstance(item, dict):
-                old_names.add(item.get("name"))
-            elif isinstance(item, str):
-                old_names.add(item)
+    new_found = False
 
-        new_found = False
+    for quest in current:
 
-        for quest in current:
+        if quest["name"] not in old_names:
 
-            if quest["name"] not in old_names:
-
-                message = f"""🚨 NEW QUEST
+            message = f"""🚨 NEW QUEST
 
 📌 {quest['name']}
 
 ⭐ XP : {quest['xp']}
 
-🔗 https://zealy.io/cw/minebit/questboard"""
+https://zealy.io/cw/minebit/questboard"""
 
-                send_message(message)
+            send_message(message)
 
-                print(f"✅ Alert Sent -> {quest['name']}")
+            print("✅ Alert:", quest["name"])
 
-                new_found = True
+            new_found = True
 
-        with open("storage.json", "w", encoding="utf-8") as f:
-            json.dump(current, f, indent=4, ensure_ascii=False)
+    with open("storage.json", "w", encoding="utf-8") as f:
+        json.dump(current, f, indent=4, ensure_ascii=False)
 
-        if not new_found:
-            print("✔ No New Quest")
+    if not new_found:
+        print("✔ No New Quest")
 
+if __name__ == "__main__":
+    try:
+        main()
     except Exception:
-        print("❌ Error occurred:")
         traceback.print_exc()
 
-    print("⏳ Checking again in 2 minutes...\n")
-    time.sleep(120)
+    print("✅ Finished")
